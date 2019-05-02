11bbcf6bcf226f55ee75e1dbce7aa03b2b96f84c
Vladimir Pankov <vladap@fiord.com>
2019-05-02 23:14:25 +0300

    Use wai-app-static to implement mxgraph's library file server.

--- src/JGraph/CDN/MxGraph.hs
+++ src/JGraph/CDN/MxGraph.hs
@@ -3,31 +3,8 @@ module JGraph.CDN.MxGraph
 ( jGraphCdnMxGraphApplication
 ) where
 
-import System.FilePath
-
-import Network.Wai
-import Network.HTTP.Types
-
-import qualified Data.Text as Text
-import qualified Data.Text.Encoding as TextEncoding
+import Network.Wai (Application)
+import Network.Wai.Application.Static (staticApp, defaultFileServerSettings)
 
 jGraphCdnMxGraphApplication :: FilePath -> Application
-jGraphCdnMxGraphApplication mxGraphFilePath = \request respond ->
-  respond $
-    if requestMethod request == methodGet
-      then let
-        filePath = mxGraphFilePath ++ Text.unpack (TextEncoding.decodeUtf8 (rawPathInfo request)) 
-      in responseFile
-           status200
-           [
-             ( "Content-Type"
-             , case takeExtension filePath of
-                 ".css"  -> "text/css"
-                 ".html" -> "text/html"
-                 ".js"   -> "application/javascript"
-                 _       -> "text/plain"
-             )
-           ]
-           filePath
-           Nothing
-      else responseLBS status400 [] ""
+jGraphCdnMxGraphApplication = staticApp . defaultFileServerSettings
